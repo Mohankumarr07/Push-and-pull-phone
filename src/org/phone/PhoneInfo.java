deb03329292cbe35fdc09f21cf123938c6e7f571
Mohankumarr07 <mohankumarr07@gmail.com>
2023-02-03 14:01:40 +0530

    push to github

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,29 @@
+package org.phone;
+
+public class PhoneInfo {
+	private void phoneName() {
+		System.out.println("Phone Name = Apple");
+		
+	}
+	private void phoneModelNumber() {
+		System.out.println("Phone Model Number = 13 PRO");
+	}
+	
+	private void phoneCamara() {
+		System.out.println("Phone Camara = 50MP");
+	}
+	
+	
+	public static void main(String args[]) {
+		PhoneInfo P=new PhoneInfo();
+		P.phoneName();
+		P.phoneModelNumber();
+		P.phoneCamara();
+		
+	}
+	
+}
+
+
+
+
