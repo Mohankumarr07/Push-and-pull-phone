583a76033ec7ce316ebfeb25992c74c5ba23b356
Mohankumarr07 <mohankumarr07@gmail.com>
2023-02-03 14:29:52 +0530

    Mohan to push Master into Modified

--- src/org/phone/PhoneInfo.java
+++ src/org/phone/PhoneInfo.java
@@ -13,13 +13,15 @@ public class PhoneInfo {
 		System.out.println("Phone Camara = 50MP");
 	}
 	
-	
+	private void phoneStorage() {
+		System.out.println("Phone Storage = 128GB");
+	}
 	public static void main(String args[]) {
 		PhoneInfo P=new PhoneInfo();
 		P.phoneName();
 		P.phoneModelNumber();
 		P.phoneCamara();
-		
+		P.phoneStorage();
 	}
 	
 }
